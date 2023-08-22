77f4a3fa5170b25e8038a43b55d1be24410f18b7
braiaguirre <brianaguirre.sp94@gmail.com>
2023-08-22 12:53:16 -0300

    update

---
+++ 06-React-Intro/homework/02 - Integration - Vite/rick and morty/src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card(props) {
+   const {name, status, species, origin, gender, image, onClose} = props;
+   return (
+      <div>
+         <button onClick={onClose}>X</button>
+         <h2>Nombre: {name}.</h2>
+         <h2>Status: {status}.</h2>
+         <h2>Especie: {species}.</h2>
+         <h2>Género: {gender}.</h2>
+         <h2>Origen: {origin}.</h2>
+         <img src={image} alt='imagen' />
+      </div>
+   );
+}
