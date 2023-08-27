9368633131d39205423f98aaeb716a406abe44ff
braiaguirre <brianaguirre.sp94@gmail.com>
2023-08-27 20:23:32 -0300

    update

--- 06-React-Intro/homework/02 - Integration - Vite/rick and morty/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration - Vite/rick and morty/src/components/Card.jsx
@@ -1,22 +1,36 @@
 import styles from './Card.module.css'
-import {Link, useNavigate} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export default function Card(props) {
+   const [loading, setLoading] = useState(false);
    const navigate = useNavigate();
    const {id, name, status, species, origin, gender, image, onClose} = props;
 
    const navigateHandler = () => navigate(`/detail/${id}`);
 
+   useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+         setLoading(false)
+      }, 1000);
+   }, []);
+
    return (
       <div className={`${styles.card} ${styles.backgroundA}`}>
-         <button onClick={() => onClose(id)}>X</button>
-         <a onClick={navigateHandler}><h2>{name}</h2></a>
-         <h3>Status: {status}</h3>
-         <h3>Especie: {species}</h3>
-         <h3>Género: {gender}</h3>
-         <h3>Origen: {origin}</h3>
-         <img src={image} alt='imagen' onClick={navigateHandler} />
-         
+         {loading ? (
+               <div className={styles.spinner}></div>
+            ) : (
+               <>
+                  <button onClick={() => onClose(id)}>X</button>
+                  <a onClick={navigateHandler}><h2>{name}</h2></a>
+                  <h3>Status: {status}</h3>
+                  <h3>Especie: {species}</h3>
+                  <h3>Género: {gender}</h3>
+                  <h3>Origen: {origin}</h3>
+                  <img src={image} alt='imagen' onClick={navigateHandler} />
+               </>
+            )}
       </div>
    );
 }
